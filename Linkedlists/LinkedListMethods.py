153638e5bd744aea035c120b345f5c1191e26422
unknown <pardhutummala812@gmail.com>
2021-11-17 11:39:17 +0530

    insert method added

--- Linkedlists/LinkedListMethods.py
+++ Linkedlists/LinkedListMethods.py
@@ -1,23 +1,25 @@
 class Node:
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value
         self.next = None
+
     def __str__(self):
         return str(self.value)
+
 
 class LinkedList:
     def __init__(self):
         self.head = None
 
-
-    def prepend(self,value):
+    def prepend(self, value):
         if self.head == None:
             self.head = Node(value)
             return
         node = self.head
         self.head = Node(value)
         self.head.next = node
-    def append(self,value):
+
+    def append(self, value):
         if self.head == None:
             self.head = Node(value)
             return None
@@ -35,7 +37,7 @@ class LinkedList:
             head = head.next
         return list_values
 
-    def search(self,value):
+    def search(self, value):
         """
         searching for a value in linkedlist
         with time complexity of O(n)
@@ -49,7 +51,7 @@ class LinkedList:
                 return head
             head = head.next
 
-    def remove(self,value):
+    def remove(self, value):
         """
         removes node with matched value
         """
@@ -64,6 +66,7 @@ class LinkedList:
                 prev_node.next = head_node.next
             prev_node = head_node
             head_node = prev_node.next
+
     def pop_first(self):
         self.head = self.head.next
 
@@ -74,23 +77,42 @@ class LinkedList:
             count = count + 1
             head = head.next
         return count
-    
 
-
+    def insert(self, val, pos):
+        head = self.head
+        prev_node = None
+        next_node = None
+        linked_list_len = self.__len__()
+        if linked_list_len - 1 < pos:
+            while head.next:
+                head = head.next
+            head.next = Node(val)
+            return
+        if pos == 0:
+            next_node = self.head
+            self.head = Node(val)
+            self.head.next = next_node
+            return
+        i = 1
+        while i <= pos:
+            prev_node = head
+            head = prev_node.next
+            next_node = head.next
+            i = i + 1
+        prev_node.next = Node(val)
+        prev_node.next.next = head
+        return
 
 
 ll = LinkedList()
-print(len(ll))
-# ll.prepend(10)
-# ll.prepend(20)
-# ll.prepend(30)
-ll.append(40)
-ll.append(50)
-ll.append(60)
-ll.append(70)
-print(len(ll))
-print(ll.to_list())
-
-ll.pop_first()
-print(len(ll))
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.append(4)
+ll.insert(50, 0)
+ll.insert(60, 1)
+ll.insert(70, 2)
+ll.insert(80, 10)
+# ll.insert(90, 70)
+# ll.insert(100, 0)
 print(ll.to_list())
